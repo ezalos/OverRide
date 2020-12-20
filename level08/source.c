26cb19aaf32556b9586579f935fef2c0ab6dafbd
ezalos <ezalos@gmail.com>
2020-12-20 12:39:04 +0100

    lvl08 better source.c

--- level08/source.c
+++ level08/source.c
@@ -6,18 +6,29 @@
 #include <unistd.h>
 #include <string.h>
 
-char var_from_fs[100];
-char somewhere[100];
-
 void	log_wrapper(FILE *fd, char *msg, char *file)
 {
 	//sub    rsp,0x130
-	char	*res;
+	//[rbp-0x130]
+	//rcx 1st scas
 	
-	strcpy(var_from_fs, msg);
+	//[rbp-0x128]
+	//rsi 1st scas
 	
-	strlen(msg);
-	res = snprintf(somewhere, 0xfe - strlen(msg), file);
+	//[rbp-0x120]
+	//rdx/rsi strcpy
+	
+	//[rbp-0x118]
+	//fprintf rdi
+	
+	//[rbp-0x110]
+	//rdx 1st scas
+	//rdi/rdx 2nd scas
+	char	res[0x110];
+
+	strcpy(res, msg);
+	strlen(res);
+	snprintf(res, 0xfe - strlen(msg), file);
 	res[strcspn(res, "\n")] = 0;
 	fprintf(fd, "LOG: %s\n", res);
 }
@@ -30,19 +41,24 @@ int		main(int ac, char **av)
 	//	sub    rsp,0xb0
 	//	[rbp-0x88]
 	FILE		*fd_log;
+	//	[rbp-0x80]
 	FILE		*fd_user;
+	//	[rbp-0x78]
 	int			fd_backup;
+	//	[rbp-0x71]
+	char		file_octet;
+	//	[rbp-0x70]
 	char		buf[0x64];
-	int			r_int;
 
-	if (ac != 2)
-		printf("Usage: %s filename", av[0]);
-	if (0 == (fd_log = fopen(BACKUP_FILE,"w")))
+	if (ac != 2)// ac == [rbp-0x94]
+		printf("Usage: %s filename", av[0]);// av[0] == [rbp-0xa0]
+	if (0 == (fd_log = fopen(BACKUP_FILE, "w")))
 	{
 		printf("ERROR: Failed to open %s\n", BACKUP_FILE);
 		exit(0x1);
 	}
-	log_wrapper(fd_log, "Starting back up: ", av[2]);
+
+	log_wrapper(fd_log, "Starting back up: ", av[1]);//av[1] == [rbp-0xa0+0x8]
 
 	if (0 == (fd_user = fopen(av[1],"r")))
 	{
@@ -50,8 +66,8 @@ int		main(int ac, char **av)
 		exit(0x1);
 	}
 	
-	buf = BACKUP_DIR;
-	strncat(buf, av[1], 0x64 - strlen(BACKUP_DIR));
+	strcpy(buf, BACKUP_DIR); //not in asm, but makes sense
+	strncat(buf, av[1], 0x64 - strlen(buf));
 
 	if (0 == (fd_backup = open(buf, 0xc1, 0x1b0)))
 	{
@@ -59,10 +75,11 @@ int		main(int ac, char **av)
 		exit(0x1);
 	}
 	do {
-		r_int = fgetc(fd_user);
-		//write(-1, ?, 1);
-	} while (r_int != 0xff);
-	log_wrapper(fd_user, "???", buf);
+		file_octet = fgetc(fd_user);
+		write(fd_backup, &file_octet, 1);
+	} while (file_octet != 0xff);
+
+	log_wrapper(fd_user, "Finished back up ", buf);
 	fclose(fd_user);
 	close(fd_backup);
 	return (0);
